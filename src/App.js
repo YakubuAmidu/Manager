5a2a97df431221640f36be2c354319562e0ec6ba
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-01-02 01:05:53 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentDidMount() {
+    var firebaseConfig = {
+      apiKey: 'AIzaSyASeC2VO9Iebej6tCixqgwWPZ8MiHYNZjI',
+      authDomain: 'manager-f6e76.firebaseapp.com',
+      projectId: 'manager-f6e76',
+      storageBucket: 'manager-f6e76.appspot.com',
+      messagingSenderId: '156250609563',
+      appId: '1:156250609563:web:0010fa7b79f92e00d0f33e',
+      measurementId: 'G-KD2KVP003Q',
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
-      <View>
-        <Text>App</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <LoginForm />
+      </Provider>
     );
   }
 }
