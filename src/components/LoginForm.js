0308ff4e18abc6dda710adff236bf6ca3d4de41f
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-01-02 16:14:39 -0800

    Initial commit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {emailChanged, passwordChanged} from '../actions';
+import {emailChanged, passwordChanged, loginUser} from '../actions';
 import {Card, CardSection, Input, Button} from './common';
 
 class LoginForm extends Component {
@@ -10,6 +10,12 @@ class LoginForm extends Component {
 
   onPasswordChage(text) {
     this.props.passwordChanged(text);
+  }
+
+  onButtonPress() {
+    const {email, password} = this.props;
+
+    this.props.loginUser({email, password});
   }
 
   render() {
@@ -35,7 +41,7 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>Login</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
         </CardSection>
       </Card>
     );
@@ -49,6 +55,8 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, {emailChanged, passwordChanged})(
-  LoginForm,
-);
+export default connect(mapStateToProps, {
+  emailChanged,
+  passwordChanged,
+  loginUser,
+})(LoginForm);
