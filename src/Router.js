3bbc37076095b9ab41ae71c02b4d02df9875c824
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-01-03 00:49:50 -0800

    Initial commit

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import {Scene, Router} from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
-    <Router sceneStyle={{paddingTop: 65}}>
-      <Scene key="login" component={LoginForm} title="Please login" />
+    <Router>
+      <Scene key="root">
+        <Scene key="login" component={LoginForm} title="Please login" />
+        <Scene
+          key="employeeList"
+          component={EmployeeList}
+          title="Employees"
+          initial
+        />
+      </Scene>
     </Router>
   );
 };
