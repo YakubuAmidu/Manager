8ce7e922e1975c654ad79594f9536f9ccf72b772
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-01-02 22:00:50 -0800

    Initial commit

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Router, Scene} from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
+
+const RouterComponent = () => (
+  <Router>
+    <Scene key="root">
+      <Scene key="login" component={LoginForm} title="Pleas login" />
+    </Scene>
+  </Router>
+);
+export default RouterComponent;
