3c15756828a616418006b7223a56a1897f5c9ce9
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-01-04 03:05:50 -0800

    Initial commit

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -25,4 +25,14 @@ class EmployeeCreate extends Component {
   }
 }
 
+const mapStateToProps = (state) => {
+  const {name, phone, shift} = state.employeeForm;
+
+  return {
+    name,
+    phone,
+    shift,
+  };
+};
+
 export default connect(null, {employeeUpdat})(EmployeeCreate);
