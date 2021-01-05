ba2a56947c77eff48a6e9d2318f3096de6bd9441
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-01-05 02:23:19 -0800

    Initial commit

--- src/reducers/EmployeeReducer.js
+++ src/reducers/EmployeeReducer.js
@@ -1,3 +1,4 @@
+import {ActionSheetIOS} from 'react-native';
 import {EMPLOYEES_FETCH_SUCCESS} from '../actions/types';
 
 const INITIAL_STATE = {};
@@ -5,8 +6,7 @@ const INITIAL_STATE = {};
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMPLOYEES_FETCH_SUCCESS:
-      console.log(action);
-      return state;
+      return action.payload;
     default:
       return state;
   }
