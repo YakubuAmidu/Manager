91ee42024f2667686792f0f151759dbedfad9138
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-01-05 19:57:44 -0800

    Initial commit

---
+++ src/components/EmployeeForm.js
@@ -0,0 +1,60 @@
+import {compact} from 'lodash';
+import React, {Component} from 'react';
+import {View, Text, Picker} from 'react-native';
+
+class EmployeeForm extends Component {
+  render() {
+    return (
+      <View>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={this.props.name}
+            onChangeText={(value) =>
+              this.props.employeeUpdate({prop: 'name', value})
+            }
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={(value) =>
+              this.props.employeeUpdate({prop: 'phone', value})
+            }
+          />
+        </CardSection>
+
+        <CardSection style={{flexDirection: 'column'}}>
+          <Text style={styles.pickerTextStyle}></Text>
+          <Picker
+            style={{flex: 1}}
+            selectedValue={this.props.shift}
+            onValueChange={(value) =>
+              this.props.employeeUpdate({prop: 'shift', value})
+            }>
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Fridday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
+        </CardSection>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+  },
+};
+
+export default EmployeeForm;
