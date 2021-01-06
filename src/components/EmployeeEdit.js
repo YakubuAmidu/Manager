8b7678807f3d7dcce713c22e36b70b9a118cf7f6
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-01-05 20:53:12 -0800

    Initial commit

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,19 +1,33 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import EmployeeForm from './EmployeeForm';
+import {employeeUpdate} from '../actions';
 import {Card, CardSection, Input} from './common';
 
 class EmployeeEdit extends Component {
+  componentDidMount() {
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({props, value});
+    });
+  }
+
+  onButtonPress() {
+    const {name, phone, shift} = this.props;
+
+    console.log(name, phone, shift);
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button>Save changes</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Save changes</Button>
         </CardSection>
       </Card>
     );
   }
 }
 
-export default connect(null, {})(EmployeeEdit);
+export default connect(null, {employeeUpdate})(EmployeeEdit);
