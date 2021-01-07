55bc596f4765322486532035e146051a46553757
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-01-06 16:45:48 -0800

    Initial commit

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -24,4 +24,21 @@ const Confirm = ({children, visible, onAccept, onDecline}) => {
   );
 };
 
+const styles = {
+  cardSectionStyle: {
+    justifyContent: 'center',
+  },
+  textStyle: {
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center',
+    lineHeight: 40,
+  },
+  containerStyle: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    position: 'relative',
+    justifyContent: 'center',
+  },
+};
+
 export {Confirm};
