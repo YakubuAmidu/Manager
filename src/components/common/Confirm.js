98d90d3b1f15654313eaae2b1071b1c6674fe51e
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-01-06 16:50:44 -0800

    Initial commit

--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -2,17 +2,20 @@ import React from 'react';
 import {Text, View, Modal} from 'react-native';
 import {CardSection} from './CardSection';
 import {Button} from './Button';
+import {text} from 'react-native-communications';
 
 const Confirm = ({children, visible, onAccept, onDecline}) => {
+  const {cardSectionStyle, textStyle, containerStyle} = styles;
+
   return (
     <Modal
       visible={visible}
       transparent
       animationType="slide"
       onRequestClose={() => {}}>
-      <View>
-        <CardSection>
-          <Text>{children}</Text>
+      <View style={containerStyle}>
+        <CardSection style={cardSectionStyle}>
+          <Text style={textStyle}>{children}</Text>
         </CardSection>
 
         <CardSection>
